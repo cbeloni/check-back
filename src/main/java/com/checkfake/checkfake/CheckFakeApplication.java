e39e615be8b010c534d593d4fba8b116c0c1f57c
Cauê Beloni <cbeloni@gmail.com>
2019-08-03 19:58:38 -0300

    primeiro commit

---
+++ src/main/java/com/checkfake/checkfake/CheckFakeApplication.java
@@ -0,0 +1,13 @@
+package com.checkfake.checkfake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckFakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckFakeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/checkfake/checkfake/controller/MemeController.java
@@ -0,0 +1,21 @@
+package com.checkfake.checkfake.controller;
+
+import com.checkfake.checkfake.exceptions.BadRequestMeme;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+@RestController("/meme")
+public class MemeController {
+
+    @GetMapping("get")
+    public ResponseEntity<String> get(){
+        if (false){
+            throw new BadRequestMeme("Deu alguma falha!");
+        }
+        return new ResponseEntity<String>("Oi", HttpStatus.OK);
+    }
+
+}
