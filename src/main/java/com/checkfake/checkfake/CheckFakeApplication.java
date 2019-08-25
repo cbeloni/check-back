5948aa025242a880c5863cbe8e95847e82d0b23f
Cauê Beloni <cbeloni@gmail.com>
2019-08-25 19:07:36 -0300

    Inclusão salvar meme

--- src/main/java/com/checkfake/checkfake/CheckFakeApplication.java
+++ src/main/java/com/checkfake/checkfake/CheckFakeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CheckFakeApplication {
+public class w3wsCheckFakeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CheckFakeApplication.class, args);
