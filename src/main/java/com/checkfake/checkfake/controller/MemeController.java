4e7746435e23edf9facb5e575bd52dfb5a2ffa17
Cauê Beloni <cbeloni@gmail.com>
2019-09-15 09:42:45 -0300

    Add Cors

--- src/main/java/com/checkfake/checkfake/controller/MemeController.java
+++ src/main/java/com/checkfake/checkfake/controller/MemeController.java
@@ -8,10 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+
 import java.util.List;
 
 @RestController("/meme")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MemeController {
 
     @Autowired
